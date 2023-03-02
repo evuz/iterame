70d0e5bb30d89fc20df9859bab588f6975c051c0
Jesús Gómez <jesgombel@outlook.es>
2023-03-02 16:04:42 +0100

    feat: create iterame class

--- src/__tests__/iterame.test.ts
+++ src/__tests__/iterame.test.ts
@@ -1,6 +1,33 @@
-import { test, expect } from 'vitest'
+import { test, expect, describe, vi } from 'vitest'
+
 import { iterame } from '../iterame'
 
-test('Iterame', () => {
-  expect(iterame(3, 2)).toBe(5)
+describe('Iterame', () => {
+  test('should return an array', () => {
+    const set = new Set([0, 1, 2, 3, 4, 5])
+    const arr = iterame(set).toArray()
+
+    expect(arr).instanceOf(Array)
+    expect(arr).toEqual([0, 1, 2, 3, 4, 5])
+  })
+
+  test('should get the iterable', () => {
+    const set = new Set([0, 1, 2, 3, 4, 5])
+    const it = iterame(set).get()
+
+    expect(it).toBe(set)
+  })
+
+  describe('pipe', () => {
+    test('should call all functions', () => {
+      const set = new Set([0, 1, 2, 3, 4, 5])
+      const map = vi.fn()
+      const filter = vi.fn()
+
+      iterame(set).pipe(map, filter)
+
+      expect(map).toBeCalledTimes(1)
+      expect(filter).toBeCalledTimes(1)
+    })
+  })
 })
--- src/iterame.ts
+++ src/iterame.ts
@@ -1,5 +1,32 @@
-export class Iterame {}
+export type PipeableFunction<T, K> = (iterator: Iterable<T>) => Generator<K, void>
 
-export function iterame (a: number, b: number) {
-  return a + b
+export class Iterame<T> {
+  constructor (private iterator: Iterable<T>) {}
+
+  pipe (...fns: PipeableFunction<any, any>[]) {
+    while (fns.length) {
+      const fn = fns.shift()
+
+      this.iterator = fn!(this.iterator)
+    }
+
+    return this
+  }
+
+  get () {
+    return this.iterator
+  }
+
+  toArray () {
+    const arr = []
+    for (const value of this.iterator) {
+      arr.push(value)
+    }
+
+    return arr
+  }
+}
+
+export function iterame<T> (iterator: Iterable<T>) {
+  return new Iterame(iterator)
 }
