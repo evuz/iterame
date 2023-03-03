dc8ec3c49568ba2b5af1f610be4faafa0add843b
Jesús Gómez <jesgombel@outlook.es>
2023-03-03 14:25:52 +0100

    feat: add operators to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
 export * from './iterame'
+
+// Operators
+export * from './operators/at'
+export * from './operators/filter'
+export * from './operators/map'
+export * from './operators/slice'
+export * from './operators/take'
